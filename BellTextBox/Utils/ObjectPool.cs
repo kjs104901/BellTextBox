4bb71bc12aa3dd74aa3932b3219f94d1ebfaab87
Kim Jinsu <kjs104901@gmail.com>
2023-11-16 00:22:37 +0900

    Object pool

---
+++ BellTextBox/Utils/ObjectPool.cs
@@ -0,0 +1,49 @@
+﻿using Bell.Data;
+
+namespace Bell.Utils;
+
+internal interface IReusable
+{
+    void Reset();
+}
+
+internal static class ObjectPoolManager
+{
+    internal static ObjectPool<LineSub> LineSub = new();
+}
+
+internal class ObjectPool<T>
+    where T : IReusable, new()
+{
+    private const int MaxCapacity = 4096;
+    
+    private readonly Stack<T> _stack = new();
+    
+    internal T Get()
+    {
+        if (_stack.Count > 0)
+            return _stack.Pop();
+        return new T();
+    }
+    
+    internal void Return(T item)
+    {
+        if (_stack.Count < MaxCapacity)
+        {
+            item.Reset();
+            _stack.Push(item);
+        }
+    }
+    
+    internal void Return(List<T> items)
+    {
+        if (_stack.Count + items.Count < MaxCapacity)
+        {
+            foreach (T item in items)
+            {
+                item.Reset();
+                _stack.Push(item);
+            }
+        }
+    }
+}
